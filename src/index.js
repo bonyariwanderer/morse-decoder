7223211dda8889b0b01383070fc1d46605750c9e
Julia Bazaeva <truckfulofcandy@gmail.com>
2021-08-21 16:47:33 +0700

    feat: add function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let array = [];
+  let binary = [];
+  let array5 = [];  
+  let decoded = '';
+
+  for (let i = 0; i < expr.length; i += 2) {
+      binary.push(expr.slice(i, i + 2));
+  }
+  
+  for (let i = 0; i < binary.length; i++) {
+
+    if (binary[i] ==='00') {
+      array.push(' ');
+    } else if (binary[i] === '10') {
+      array.push('.');
+    } else if (binary[i] === '11') {
+      array.push('-');
+    } else if (binary[i] === '**') {
+      array.push('*');
+    }
+  }
+  
+  for (let i = 0; i < array.length; i += 5) {
+      array5.push(array.slice(i, i + 5).toString());
+  }
+  
+  let arr = array5.map(e => e.replace(/,/g, ''));
+
+  arr = arr.map(function (e) {
+    return e.trim();
+  });
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '*****') {
+      decoded += ' ';
+    } else {
+        decoded += (MORSE_TABLE[arr[i]]).toString();
+      }
+  }
+
+  return decoded;
 }
 
 module.exports = {
